24d66c3a966a321ecc5b54984ae5bc669d7474d3
SockNastre <msaqib247@hotmail.com>
2022-04-28 01:52:00 -0400

    Setting debug variables

--- bank0/Initialization/SetVariables.asm
+++ bank0/Initialization/SetVariables.asm
@@ -1,5 +1,23 @@
 
 	SetVariables:
+		;; setting the debug variables
+		LDA #$53
+		STA temp1
+		LDA #$4D
+		STA temp2
+		LDA #$3A
+		STA temp3
+		LDA #$33
+		STA temp4
+		LDA #$48
+		STA temp5
+		LDA #$45
+		STA temp6
+		LDA #$59
+		STA temp7
+		LDA #$3F
+		STA temp8
+
 		;; setting the tetrimino down/side speed variables to their
 		;; default speeds
 		LDX #TETRIMINO_DEFAULT_DOWNSPEED
