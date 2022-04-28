1411ebb10081e62b9029a7b99e2adc6725c90bca
SockNastre <msaqib247@hotmail.com>
2022-04-28 01:29:11 -0400

    Fixed up comments and optimized code

--- bank0/GraphicsUpdates/CheckTetriminoRightSide.asm
+++ bank0/GraphicsUpdates/CheckTetriminoRightSide.asm
@@ -1,28 +1,37 @@
 
 	CheckTetriminoRightSide:
+		;; going to where right side bottom block would be stored in
+		;; the nametable
 		LDA rightBottomBlockLowPointer
 		STA PPU_ADDRESS
-		STA temp2
 		LDA rightBottomBlockHighPointer
 		STA PPU_ADDRESS
-		STA temp3
 
-		LDA #$69
-		STA temp1
-
-		LDA PPU_DATA ;; discarding what was in PPU_DATA last
-
+		;; discarding last PPU data byte
 		LDA PPU_DATA
-		STA temp3
+
+		;; loading right bottom block, seeing if it equal to tile ID
+		;; 0x02 or not
+		LDA PPU_DATA
 		CMP #02
 		BEQ TetriminoRightSideBad
 
+		;; since there is no tile 0x02 (tetrimino block) then the
+		;; right space is open
 		LDA #TRUE
 		STA isRightSpaceOpen
 
+		;; jumping to end of check, since we are done
 		JMP CheckTetriminoRightSideDone
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; when the right side of the tetrimino is bad (not open)
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	TetriminoRightSideBad:
+		;; since there is tile 0x02 (tetrimino block) then the right
+		;; space is not open
 		LDA #FALSE
 		STA isRightSpaceOpen
-		JMP CheckTetriminoRightSideDone
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; done checking tetrimino's right side
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	CheckTetriminoRightSideDone:
