cef14e81d40eb2284d0527815c2c23f0ad47ef0f
SockNastre <msaqib247@hotmail.com>
2022-04-28 01:27:29 -0400

    Fixed up comments and optimized code

--- bank0/GraphicsUpdates/CheckTetriminoLeftSide.asm
+++ bank0/GraphicsUpdates/CheckTetriminoLeftSide.asm
@@ -1,23 +1,37 @@
 
 	CheckTetriminoLeftSide:
+		;; going to where left side bottom block would be stored in
+		;; the nametable
 		LDA leftBottomBlockLowPointer
 		STA PPU_ADDRESS
 		LDA leftBottomBlockHighPointer
 		STA PPU_ADDRESS
 
-		LDA PPU_DATA ;; discarding what was in PPU_DATA last
-
+		;; discarding last PPU data byte
 		LDA PPU_DATA
-		STA temp3
+
+		;; loading left bottom block, seeing if it equal to tile ID
+		;; 0x02 or not
+		LDA PPU_DATA
 		CMP #02
 		BEQ TetriminoLeftSideBad
 
+		;; since there is no tile 0x02 (tetrimino block) then the
+		;; left space is open
 		LDA #TRUE
 		STA isLeftSpaceOpen
 
+		;; jumping to end of check, since we are done
 		JMP CheckTetriminoLeftSideDone
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; when the left side of the tetrimino is bad (not open)
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	TetriminoLeftSideBad:
+		;; since there is tile 0x02 (tetrimino block) then the left
+		;; space is not open
 		LDA #FALSE
 		STA isLeftSpaceOpen
-		JMP CheckTetriminoLeftSideDone
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; done checking tetrimino's left side
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	CheckTetriminoLeftSideDone:
